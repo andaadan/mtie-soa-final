5dadd613474cd0ab332c6be5c370aceb9e3136dd
unknown <andaadan@gmail.com>
2021-05-06 10:47:37 -0500

    Nueva esperanza

--- volumes/logstash/config/queries/incremental.sql
+++ volumes/logstash/config/queries/incremental.sql
@@ -1,8 +1,10 @@
 SELECT
-	j.journal_id, j.action_type, j.isbn,
-	b.title, b.authors, b.publication_date
-FROM books.books_journal j
-LEFT JOIN books.books b ON b.isbn = j.isbn
+	j.journal_id, j.action_type, 
+	j.factura, b.codCliente, b.fechaFactura, 
+	b.formaPago, b.subtotal, b.iva, b.total,
+	b.planta, b.vendedor
+FROM sales.facturas_journal j
+LEFT JOIN sales.facturas b ON b.factura = j.factura
 WHERE j.journal_id > :sql_last_value
 	AND j.action_time < NOW()
 ORDER BY j.journal_id
